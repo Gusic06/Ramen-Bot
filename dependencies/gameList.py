6ed655e619b60ebb1b06bf951b9d7029dc47f7ec
gusic <97785573+Gusic06@users.noreply.github.com>
2022-11-12 01:28:01 +0000

    Update gameList.py

--- dependencies/gameList.py
+++ dependencies/gameList.py
@@ -1,1 +1,15 @@
-gameList = ("destiny", "ultrakill", "persona", "risk of rain", "dauntless", "monster hunter world", "outriders", "overwatch", "skyrim", "hollow knight", "cult of the lamb", "devil may cry 5", "witcher 3")
+gameList = (
+    "destiny",
+    "ultrakill",
+    "persona",
+    "risk of rain",
+    "dauntless",
+    "monster hunter world",
+    "outriders",
+    "overwatch",
+    "skyrim",
+    "hollow knight",
+    "cult of the lamb",
+    "devil may cry 5",
+    "witcher 3"
+)
